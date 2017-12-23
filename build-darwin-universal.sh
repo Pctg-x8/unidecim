7fccd410dc22428fe5dfa24c0c30b9b30a04725d
S.Percentage <Syn.Tri.Naga@gmail.com>
2017-12-23 14:01:05 +0900

    changed export name fitting for Unity

--- build-darwin-universal.sh
+++ build-darwin-universal.sh
@@ -6,7 +6,7 @@ X86_TOOLCHAIN="nightly-i686-apple-darwin"
 CARGO_OUTPUT="$TARGET_DIR/libAudioPlugin_Unidecim.dylib"
 X86_TARGET="$TARGET_DIR/libAudioPlugin_Unidecim_x86.dylib"
 X64_TARGET="$TARGET_DIR/libAudioPlugin_Unidecim_x64.dylib"
-FAT_TARGET="$TARGET_DIR/AudioPlugin_Unidecim.dylib"
+FAT_TARGET="$TARGET_DIR/AudioPlugin_Unidecim.bundle"
 
 rustup run $X64_TOOLCHAIN cargo build --release && mv $CARGO_OUTPUT $X64_TARGET
 rustup run $X86_TOOLCHAIN cargo build --release && mv $CARGO_OUTPUT $X86_TARGET
